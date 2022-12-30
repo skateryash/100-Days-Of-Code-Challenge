6528aefb4c52da667fb62f4ff24bb9410d4aa1f4
Yash Chaudhary <ygcjal@gmail.com>
2022-12-30 19:28:16 +0530

    Day 63

---
+++ Day 63/practice.py
@@ -0,0 +1,75 @@
+import sqlite3
+
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+
+# cursor.execute("CREATE TABLE books "
+#                "(id INTEGER PRIMARY KEY, "
+#                "title varchar(250) NOT NULL UNIQUE, "
+#                "author varchar(250) NOT NULL, "
+#                "rating FLOAT NOT NULL)"
+#                )
+
+# cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
+all_books = cursor.execute("SELECT * FROM books")
+print(all_books)
+db.commit()
+
+# from flask import Flask, session
+# from flask_sqlalchemy import SQLAlchemy
+#
+# app = Flask(__name__)
+# # app.app_context().push()
+#
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books-collection.db"
+# # Optional: But it will silence the deprecation warning in the console.
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
+#
+#
+# # CREATE TABLE
+# class Book(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     title = db.Column(db.String(250), unique=True, nullable=False)
+#     author = db.Column(db.String(250), nullable=False)
+#     rating = db.Column(db.Float, nullable=False)
+#
+#     # Optional: this will allow each book object to be identified by its title when printed.
+#     def __repr__(self):
+#         return f'<Book {self.title}>'
+#
+#
+# db.create_all()
+
+# CREATE RECORD
+# new_book = Book(id=1, title='Harry Potter', author='J.K. Rowling', rating='9.3')
+# db.session.add(new_book)
+# db.session.commit()
+
+# Read All Records
+# all_books = session.query(Book).all()
+# print(all_books)
+# db.session.commit()
+
+# Read A Particular Record By Query
+# book = Book.query.filter_by(title="Harry Potter").first()
+# db.session.commit()
+
+# # Update A Particular Record By Query
+# book_to_update = Book.query.filter_by(title="Harry Potter").first()
+# book_to_update.title = "Harry Potter and the Chamber of Secrets"
+# db.session.commit()
+
+
+# # Update A Record By PRIMARY KEY
+# book_id = 1
+# book_to_update = Book.query.get(book_id)
+# book_to_update.title = "Harry Potter and the Goblet of Fire"
+# db.session.commit()
+#
+#
+# # Delete A Particular Record By PRIMARY KEY
+# book_id = 1
+# book_to_delete = Book.query.get(book_id)
+# db.session.delete(book_to_delete)
+# db.session.commit()
